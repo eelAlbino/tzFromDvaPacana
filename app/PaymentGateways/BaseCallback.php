e1aa5d6696fbd8e4acb2bd8f7bb685922305a1b8
Igor Kopylov <kopylov.i@claramente.ru>
2023-11-16 18:19:17 +0300

    Feat: Использование дневной статистики для проверки на дневной лимит

--- app/PaymentGateways/BaseCallback.php
+++ app/PaymentGateways/BaseCallback.php
@@ -4,6 +4,9 @@ namespace App\PaymentGateways;
 
 use App\Models\Payment;
 use App\Models\PaymentGateway;
+use App\Models\PaymentGatewayDayStatistic;
+use Carbon\Carbon;
+use Exception;
 
 abstract class BaseCallback
 {
@@ -12,8 +15,19 @@ abstract class BaseCallback
      */
     protected PaymentGateway $gateway;
 
+    /**
+     * Элементы дневной статистики для текущей модели шлюза
+     */
+    private array $dayStatisticItems = [];
+
+    /**
+     * Проверять ли дневной лимит
+     */
+    private bool $useLimitDay;
+
     function __construct(PaymentGateway $gateway) {
         $this->gateway = $gateway;
+        $this->useLimitDay = $gateway->callback_limit_day > 0;
     }
 
     /**
@@ -29,9 +43,59 @@ abstract class BaseCallback
      * @param Payment $payment
      */
     public function sendUpdatedPayment(Payment $payment) {
-        // check limit (daily count)
+        $dateNow = Carbon::now();
+        if ($this->useLimitDay && !$this->checkLimitDay($dateNow)) {
+            throw new Exception('Исчерпан лимит на дневную отправку данных');
+        }
         $this->processSendUpdatedPayment($payment);
-        // inc daily count
+        if ($this->useLimitDay) {
+            $this->incAttemptDay($dateNow);
+        }
     }
 
+    /**
+     * Проверка дневного лимита на отправку данных
+     * @param Carbon $date
+     * @return bool
+     */
+    private function checkLimitDay(Carbon $date): bool
+    {
+        $limitDay = $this->gateway->callback_limit_day;
+        if ($limitDay <= 0) {
+            return true;
+        }
+        $statisticElem = $this->dayStatisticElem($date);
+        return ($statisticElem->attempts_count < $limitDay);
+    }
+
+    /**
+     * Отметка отправки данных за текущий день
+     * @param Carbon $date
+     */
+    private function incAttemptDay(Carbon $date) {
+        $statisticElem = $this->dayStatisticElem($date);
+        $statisticElem->fill([
+            'attempts_count' => ($statisticElem->attempts_count + 1)
+        ])->save();
+    }
+
+    /**
+     * Получение элемента статистики за день для заданного шлюза
+     * @param Carbon $date
+     * @return PaymentGatewayDayStatistic
+     */
+    private function dayStatisticElem(Carbon $date): PaymentGatewayDayStatistic
+    {
+        $dateFormat = $date->format('Y-m-d');
+        if (!isset($this->dayStatisticItems[ $dateFormat ])) {
+            $item = PaymentGatewayDayStatistic::whereDate('date', '=', $date)->where([
+                'gateway_id' => $this->gateway->id
+            ])->first();
+            if (!$item) {
+                $item = PaymentGatewayDayStatistic::create($searchData);
+            }
+            $this->dayStatisticItems[ $dateFormat ] = $item;
+        }
+        return $this->dayStatisticItems[ $dateFormat ];
+    }
 }
