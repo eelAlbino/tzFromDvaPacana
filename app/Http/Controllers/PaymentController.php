e7eea049e7925fbb7dc513f6f375c17afd3df33c
Igor Kopylov <kopylov.i@claramente.ru>
2023-11-16 03:31:54 +0300

    Feat: Api метод /api/payments/{id}/touch
    Небольшой api метод для триггера события обновления элемента платежа с указанным id.

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Payment;
+
+class PaymentController extends Controller
+{
+
+    /**
+     * Триггер обновления элемента оплаты для отправки данных элемента на шлюз
+     * 
+     * @param int $id
+     */
+    public function touch(int $id)
+    {
+        $result = [
+            'success' => false,
+            'errors' => []
+        ];
+        $pageCode = 200;
+        do {
+            $payment = Payment::find($id);
+            if (!$payment) {
+                $result['errors'][]= [
+                    'code' => 'not_found',
+                    'message' => 'Элемент не найден'
+                ];
+                $pageCode = 404;
+                break;
+            }
+            if (!$payment->touch()) {
+                $result['errors'][]= [
+                    'code' => 'not_updated',
+                    'message' => 'Элемент не был обновлен'
+                ];
+                $pageCode = 500;
+                break;
+            }
+            $result['success'] = true;
+        } while (false);
+        return response()->json($result, $pageCode);
+    }
+}
