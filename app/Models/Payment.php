b05e5398f3f09fe64dc7d5987997c09a4d0cbbf0
Igor Kopylov <kopylov.i@claramente.ru>
2023-11-16 16:45:32 +0300

    Feat: Модель оплаты. Добавление методов для получения связанных элементов

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -3,11 +3,32 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
 use App\Contracts\Swagger\Models\IPaymentSwagger;
+use App\Events\PaymentUpdated;
 
 class Payment extends Model implements IPaymentSwagger
 {
     use HasFactory;
 
+    /**
+     * Шлюз оплаты
+     */
+    public function gateway(): BelongsTo
+    {
+        return $this->belongsTo(PaymentGateway::class, 'gateway_id');
+    }
+
+    /**
+     * Покупатель
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+
+    protected $dispatchesEvents = [
+        'updated' => PaymentUpdated::class,
+    ];
 }
